4a3eb3198757fd9adf6fad0345a2265c7df3d6c0
sosoUtopia <alvinxya@gmail.com>
2023-02-07 08:40:51 -0500

    css update

--- src/components/accomplishments/Skills.jsx
+++ src/components/accomplishments/Skills.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import "./skills.css";
+import "./accomplishments.css";
 import Technical from "./Technical";
 import Office from "./Office";
 
-const Skills = () => {
+const Accomplishments = () => {
   return (
-    <section className="skills section">
-        <h2 className="section-title">Skills</h2>
+    <section className="Accomplishments section">
+        <h2 className="section-title">Achievements</h2>
         <span className="section-subtitle">What I currently have</span>
         
-        <div className="skills-container container grid">
+        <div className="Accomplishments-container container grid">
             <Technical />
             <Office />
         </div>
@@ -17,4 +17,4 @@ const Skills = () => {
   )
 }
 
-export default Skills
+export default Accomplishments
